7b90ac5c383aca45d4e11ac556e29743f1307b48
dominika15 <dominikaa.b@onet.pl>
2017-07-07 21:40:40 +0200

    Ready only view in second section - numbers

--- js/script.js
+++ js/script.js
@@ -9,4 +9,40 @@ $(document).ready(function() {
 
 
 
+/* NUMBER */
+/*
+
+$(function($, win) {
+    $.fn.inViewport = function(cb) {
+        return this.each(function(i,el){
+            function visPx(){
+                var H = $(this).height(),
+                    r = el.getBoundingClientRect(),
+                    t=r.top,
+                    b=r.bottom;
+                return cb.call(el, Math.max(0, t>0? H-t : (b<H?b:H)));
+            } visPx();
+            $(win).on("resize scroll", visPx);
+        });
+    };
+}(jQuery, window));
+
+jQuery(function($) {
+    $(".numbers__circle--count").inViewport(function(px) {
+
+        if(px>0 && !this.visibilityFlag) {
+            this.visibilityFlag = true;
+            $(this).prop('Counter',0).animate({
+                Counter: $(this).text()
+            }, {
+                duration: 3000,
+                step: function (now) {
+                    $(this).text(Math.ceil(now));
+                }
+            });
+        }
+    });
+});*/
+
+
 
