57a26ca5f235bdeaed418ab1ffd94e129d95c8a0
dominika15 <dominikaa.b@onet.pl>
2017-07-07 13:17:21 +0200

    Ready menu in first section

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/* NAVBAR */
+
+$(document).ready(function() {
+    $(".slider__header--menu").click(function(){
+        $(this).toggleClass("active");
+        $(".slider__header--menu--text").toggleClass("hidden");
+    });
+});
+
+
+
+
