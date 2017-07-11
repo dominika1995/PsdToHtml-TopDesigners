8f266a0e3c2473373b451c206cc3052047716c2e
dominika15 <dominikaa.b@onet.pl>
2017-07-11 16:04:00 +0200

    Ready menu

--- js/script.js
+++ js/script.js
@@ -1,40 +1,48 @@
-/* NAVBAR */
 
-$(document).ready(function() {
-    $(".slider__header--menu").click(function(){
-        $(this).toggleClass("active");
-        $(".slider__header--menu--text").toggleClass("hidden");
+$(document).ready(function () {
+
+
+    /* NAVBAR */
+
+
+    var isActive = false;
+    $('.js-menu').on('click', function () {
+        if (isActive) {
+            $(this).removeClass('active');
+            $('body').removeClass('menu-open');
+        } else {
+            $(this).addClass('active');
+            $('body').addClass('menu-open');
+        }
+        isActive = !isActive;
     });
 
 
+    /* NUMBER */
 
 
-/* NUMBER */
-
-    $(function($, win) {
-        $.fn.inViewport = function(cb) {
-            return this.each(function(i,el){
-                function visPx(){
+    $(function ($, win) {
+        $.fn.inViewport = function (cb) {
+            return this.each(function (i, el) {
+                function visPx() {
                     var H = $(this).height(),
                         r = el.getBoundingClientRect(),
-                        t=r.top,
-                        b=r.bottom;
-
-                    return cb.call(el, Math.max(0, t>0? H-t : (b<H?b:H)));
-                } visPx();
+                        t = r.top,
+                        b = r.bottom;
+                    return cb.call(el, Math.max(0, t > 0 ? H - t : (b < H ? b : H)));
+                }
+                visPx();
                 $(win).on("resize scroll", visPx);
             });
         };
     }(jQuery, window));
 
-    jQuery(function($) {
-        $(".numbers__circle--count").inViewport(function(px) {
-
-
-            if(px>0 && !this.visibilityFlag) {
+    jQuery(function ($) {
+        $(".numbers__circle--count").inViewport(function (px) {
+            if (px > 0 && !this.visibilityFlag) {
                 this.visibilityFlag = true;
-                $( ".numbers__circle--colors" ).addClass("numbers__circle--animation");
-                $(this).prop('Counter',0).animate({
+                $(".numbers__circle--colors").addClass("numbers__circle--animation");
+                $(this).prop('Counter', 0).animate({
                     Counter: $(this).text()
                 }, {
                     duration: 5000,
@@ -44,45 +52,8 @@ $(document).ready(function() {
                 });
             }
         });
-
     });
-
-
-
-
-/*
-
-$(function($, win) {
-    $.fn.inViewport = function(cb) {
-        return this.each(function(i,el){
-            function visPx(){
-                var H = $(this).height(),
-                    r = el.getBoundingClientRect(),
-                    t=r.top,
-                    b=r.bottom;
-                return cb.call(el, Math.max(0, t>0? H-t : (b<H?b:H)));
-            } visPx();
-            $(win).on("resize scroll", visPx);
-        });
-    };
-}(jQuery, window));
-
-jQuery(function($) {
-    $(".numbers__circle--count").inViewport(function(px) {
-
-        if(px>0 && !this.visibilityFlag) {
-            this.visibilityFlag = true;
-            $(this).prop('Counter',0).animate({
-                Counter: $(this).text()
-            }, {
-                duration: 3000,
-                step: function (now) {
-                    $(this).text(Math.ceil(now));
-                }
-            });
-        }
-    });
-});*/
-
 });
+
+
 
